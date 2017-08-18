226bb3ebff1a03dfc4c4045375baaa4e6acdc64c
HostiL <grbovic.pro@gmail.com>
2017-08-18 19:25:59 +0200

    Adding my existing site.

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var user = "";
+
+while (true) {
+	var user = prompt("Quel est ton prénom?");
+  
+  if (user) {
+  	break;
+  }
+}
+	alert ("Bienvenue sur mon site " + user + "." + "\nJe débute en programmation, donc soit indulgent sur le fonctionnement. \n\n© Aleksandar Grbovic développeur en alternance. \nItescia | CodingFactory.fr | 2 ans \n\nBonne navigation.");
